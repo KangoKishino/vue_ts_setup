678cd8bfa80ef688eed4859164293db31e8aefb8
your git username <your git email>
2021-06-20 20:21:14 +0900

    vue_ts_setup

---
+++ src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home">
+    <p>{{ greetText }}</p>
+    <p>挨拶した回数：{{ count }}回</p>
+    <p v-if="isRegulars">いつもありがとうございます</p>
+    <p>
+      <MyButton class="my-button" :greet="greetText" @click="onMyButtonClicked">挨拶する</MyButton>
+    </p>
+    <p>
+      <ResetButton v-model="greetText"></ResetButton>
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Watch, Vue } from 'vue-property-decorator';
+import MyButton from '@/components/MyButton.vue';
+import ResetButton from '@/components/ResetButton.vue';
+
+@Component({
+  components: {
+    MyButton,
+    ResetButton,
+  },
+})
+export default class Home extends Vue {
+  private count = 0;
+  public greetText = 'Hello';
+
+  public get isRegulars(): boolean {
+    return this.count >= 5;
+  }
+
+  @Watch('count')
+  public countChanged() {
+    if (this.count === 5) {
+      alert('常連になりました');
+    }
+  }
+
+  public onMyButtonClicked(count: number) {
+    this.count = count;
+    this.greetText = 'こんにちは';
+  }
+}
+</script>
